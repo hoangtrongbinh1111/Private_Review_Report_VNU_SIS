510e71536e06afa5d73d986aad44ec832aaf55e4
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2024-05-31 11:45:18 +0700

    Update

--- app/views.py
+++ app/views.py
@@ -81,7 +81,7 @@ def home():
         if currency_money is None or currency_money == '':
             continue
         try:
-            if index >= 10:
+            if index >= 6:
                 if int(currency_money) == 0:
                     continue
                 if currency_money is not None and currency_money != '':
