ac3178204cb15a68003b36812948c71bebcdfc06
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2024-07-02 13:50:27 +0700

    ud

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   id integer primary key autoincrement,
+  code string not null,
   name string not null,
   uuid string not null,
   rowIndex string not null
