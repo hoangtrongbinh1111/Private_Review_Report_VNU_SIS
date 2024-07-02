60cd7c779116b2436a8b947ff6de21d8d55655dd
Nguyễn Hoàng Hiệp <hoanghiepdn97@gmail.com>
2024-07-02 14:04:57 +0700

    ud

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-import sqlite3
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-app.config.from_object(__name__)
-from app import views
 # Path to your schema.sql file
 SCHEMA = os.path.join(basedir, 'schema.sql')
 
 def create_database():
-    conn = sqlite3.connect(os.path.join(basedir, 'mydatabase.db'))
-    cursor = conn.cursor()
-    with open(SCHEMA, 'r') as f:
-        schema_sql = f.read()
-    cursor.executescript(schema_sql)
-    conn.commit()
-    conn.close()
+    if not os.path.exists(os.path.join(basedir, 'mydatabase.db')):
+        with app.app_context():
+            conn = db.engine.raw_connection()
+            cursor = conn.cursor()
+            with open(SCHEMA, 'r') as f:
+                schema_sql = f.read()
+            cursor.executescript(schema_sql)
+            conn.commit()
+            conn.close()
+            print("Database created successfully from schema.sql.")
 
 @app.before_first_request
 def initialize_database():
     create_database()
-    print("Database created successfully from schema.sql.")
+
+from app import views
